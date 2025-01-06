88417b5e0d598bec3370e69088d3b0ec07d3e980
Samira <samira08@gmail.com>
2025-01-06 23:29:38 +0100

    correction

--- Kasa-app/src/compoments/AnnoncesGrid.jsx
+++ Kasa-app/src/compoments/AnnoncesGrid.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 import { useState, useEffect } from 'react';
 import logements from '../data/logements.json';
 import '../styles/AnnoncesGrid.css';
